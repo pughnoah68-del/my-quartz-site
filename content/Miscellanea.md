1137d1af1f0445dd2ed683b290f5688b62b7e35a
Noah Pugh <noahpugh@noahs-air.home>
2026-08-04 16:52:44 +0100

    Remove folder pages

--- content/Miscellanea.md
+++ content/Miscellanea.md
@@ -3,5 +3,4 @@ title: "Miscellanea"
 ---
 
 1. Model Theory and Groups, my MMath project at St Andrews, [pdf](pdfs/Diss.pdf)
-
 2. CV, last updated August 2026, [pdf](pdfs/CV%20August%202026.pdf)
--- quartz.config.ts
+++ quartz.config.ts
@@ -87,7 +87,6 @@ const config: QuartzConfig = {
       Plugin.AliasRedirects(),
       Plugin.ComponentResources(),
       Plugin.ContentPage(),
-      Plugin.FolderPage(),
       Plugin.TagPage(),
       Plugin.ContentIndex({
         enableSiteMap: true,
